8254533d0487f8675b2b129d5a77769c10d2cd6d
Glotrera <franciscolcpereira99@gmail.com>
2025-07-22 11:46:11 +0100

    ano comentario.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
+ano: int = input("Qual é o seu ano de nascimento? ") #pedir o ano de nascimento
 print(f"Olá, {nome}!")
